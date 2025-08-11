8cfa31bffa40a2f2d1e8305b5c641295dfdc5ccf
soukaina1DEV <soukaina.elfajjaj123@gmail.com>
2025-08-11 20:28:36 +0100

    third

--- week1/day1/exercices/exercice1.py
+++ week1/day1/exercices/exercice1.py
@@ -1,50 +1,50 @@
-# #exercice1
-# print("hello world\n" * 4)
+#exercice1
+print("hello world\n" * 4)
 
-# #exercice2
-# result = 99**3*8
-# print(result)
+#exercice2
+result = 99**3*8
+print(result)
 
-# #exercice3 
-# my_name = "soukaina"
-# user_name = input("enter your name: " )
-# if my_name == user_name:
-#    print("Oh we have the same name")
-# else:
-#    print("nice to meet you " + user_name)
+#exercice3 
+my_name = "soukaina"
+user_name = input("enter your name: " )
+if my_name == user_name:
+   print("Oh we have the same name")
+else:
+   print("nice to meet you " + user_name)
 
-# #exercice4
-# user_height = int(input("enter your height: " ))
-# if user_height > 145:
-#    print("you are tall enough to ride.")
-# else:
-#    print("you need to grow some more to ride.")
+#exercice4
+user_height = int(input("enter your height: " ))
+if user_height > 145:
+   print("you are tall enough to ride.")
+else:
+   print("you need to grow some more to ride.")
 
-# #exercice5
-# my_fav_numbers = {5,7,2,9}
-# my_fav_numbers.add(4)
-# my_fav_numbers.add(6)
-# my_fav_numbers.pop()
-# print(my_fav_numbers)
+#exercice5
+my_fav_numbers = {5,7,2,9}
+my_fav_numbers.add(4)
+my_fav_numbers.add(6)
+my_fav_numbers.pop()
+print(my_fav_numbers)
 
-# friend_fav_numbers = {1,3,8}
-# our_fav_numbers = my_fav_numbers.union(friend_fav_numbers) 
-# print(our_fav_numbers)
+friend_fav_numbers = {1,3,8}
+our_fav_numbers = my_fav_numbers.union(friend_fav_numbers) 
+print(our_fav_numbers)
 
-# # exercice6
-# answer = print("yes, Tuples are immutable lists, which means items can’t be changed.")
+# exercice6
+answer = print("yes, Tuples are immutable lists, which means items can’t be changed.")
 
-# #exercice7
-# basket = ["Banana", "Apples", "Oranges", "Blueberries"]
-# basket.remove("Banana")
-# basket.remove("Blueberries")
-# basket.append("Kiwi")
-# basket.insert(0,"Apples")
-# print(basket)
-# count_apples = basket.count("Apples")
-# print(count_apples)
-# basket.clear()
-# print(basket)
+#exercice7
+basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+basket.remove("Banana")
+basket.remove("Blueberries")
+basket.append("Kiwi")
+basket.insert(0,"Apples")
+print(basket)
+count_apples = basket.count("Apples")
+print(count_apples)
+basket.clear()
+print(basket)
 
 # exercice8
 sandwich_orders = ["Tuna sandwich", "Pastrami sandwich", "Avocado sandwich", "Pastrami sandwich", "Egg sandwich", "Chicken sandwich", "Pastrami sandwich"]
