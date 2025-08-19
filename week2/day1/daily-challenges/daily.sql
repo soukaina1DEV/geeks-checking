e9bfdca3d545330bd976232087d4a9e9327de8ce
soukaina1DEV <soukaina.elfajjaj123@gmail.com>
2025-08-19 15:05:24 +0100

    update week2

---
+++ week2/day1/daily-challenges/daily.sql
@@ -0,0 +1,6 @@
+SELECT * FROM actors;
+SELECT COUNT(*) FROM actors;
+INSERT INTO actors (first_name, last_name )VALUES('soukaina');
+
+-- "3lach tal3at erreur , 7it ila darna jouj dyal les attributes ftable actors khasna ninserew jouj dyal les valeurs, ila n9asna ghir valeur wa7da ghatala3 erreur "
+
