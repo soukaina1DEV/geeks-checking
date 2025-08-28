aee9eb2302b02596b76436b5f18b7f6501c6f24e
soukaina1DEV <soukaina.elfajjaj123@gmail.com>
2025-08-29 00:32:11 +0100

    update

---
+++ week2/day5/Flask_F_Stack_Recipes/database/index.py
@@ -0,0 +1,21 @@
+from psycopg2 import connect
+from psycopg2.extras import RealDictConnection
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def connect_to_db():
+    try:
+        conn = connect(
+            host='ep-calm-sky-aduulw9g-pooler.c-2.us-east-1.aws.neon.tech',
+            database='neondb',
+            user='neondb_owner',
+            password='npg_AcgkdTtr1PB2',
+            sslmode='require',
+        )
+        return conn
+    except Exception as e:
+        print(e)
+        return None
