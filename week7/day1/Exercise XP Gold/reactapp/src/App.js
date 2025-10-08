d9d76ce876c1ccfd67c33a84f06294ec237f6728
soukaina1DEV <soukaina.elfajjaj123@gmail.com>
2025-10-08 12:34:09 +0100

    week7 dya1

--- week7/day1/Exercise XP Gold/reactapp/src/App.js
+++ week7/day1/Exercise XP Gold/reactapp/src/App.js
@@ -1,40 +1,40 @@
 // Exercise 1 : Bootstrap
 
-// import React from 'react'
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import BootstrapCard from "./BootstrapCard";
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import BootstrapCard from "./BootstrapCard";
 
-// function App() {
-//   const celebrities = [
-//     {
-//       title: "Bob Dylan",
-//       imageUrl:
-//         "https://miro.medium.com/max/4800/1*_EDEWvWLREzlAvaQRfC_SQ.jpeg",
-//       buttonLabel: "Go to Wikipedia",
-//       buttonUrl: "https://en.wikipedia.org/wiki/Bob_Dylan",
-//       description:
-//         "Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer/songwriter, author, and artist who has been an influential figure in popular music and culture for more than five decades.",
-//     },
-//     {
-//       title: "McCartney",
-//       imageUrl:
-//         "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Paul_McCartney_in_October_2018.jpg/240px-Paul_McCartney_in_October_2018.jpg",
-//       buttonLabel: "Go to Wikipedia",
-//       buttonUrl: "https://en.wikipedia.org/wiki/Paul_McCartney",
-//       description:
-//         "Sir James Paul McCartney CH MBE (born 18 June 1942) is an English singer, songwriter, musician, composer, and record and film producer who gained worldwide fame as co-lead vocalist and bassist for the Beatles.",
-//     },
-//   ];
+function App() {
+  const celebrities = [
+    {
+      title: "Bob Dylan",
+      imageUrl:
+        "https://miro.medium.com/max/4800/1*_EDEWvWLREzlAvaQRfC_SQ.jpeg",
+      buttonLabel: "Go to Wikipedia",
+      buttonUrl: "https://en.wikipedia.org/wiki/Bob_Dylan",
+      description:
+        "Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer/songwriter, author, and artist who has been an influential figure in popular music and culture for more than five decades.",
+    },
+    {
+      title: "McCartney",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Paul_McCartney_in_October_2018.jpg/240px-Paul_McCartney_in_October_2018.jpg",
+      buttonLabel: "Go to Wikipedia",
+      buttonUrl: "https://en.wikipedia.org/wiki/Paul_McCartney",
+      description:
+        "Sir James Paul McCartney CH MBE (born 18 June 1942) is an English singer, songwriter, musician, composer, and record and film producer who gained worldwide fame as co-lead vocalist and bassist for the Beatles.",
+    },
+  ];
 
-//   return (
-//     <div className="d-flex justify-content-center flex-wrap">
-//       <BootstrapCard {...celebrities[0]} />
-//       <BootstrapCard {...celebrities[1]} />
-//     </div>
-//   );
-// }
+  return (
+    <div className="d-flex justify-content-center flex-wrap">
+      <BootstrapCard {...celebrities[0]} />
+      <BootstrapCard {...celebrities[1]} />
+    </div>
+  );
+}
 
-// export default App;
+export default App;
 
 
 
